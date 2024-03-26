b1e2dd7d753492e1929f5d53d7f7448b9f422335
basilalias0 <basilalyas2000@gmail.com>
2024-03-26 09:30:36 +0530

    firebase config changed

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -5,13 +5,13 @@ import "firebase/compat/storage";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCYR_rf6w4xdI-_Uc44gShCbUDhIsLRuXw",
-    authDomain: "olx-clone-44be2.firebaseapp.com",
-    projectId: "olx-clone-44be2",
-    storageBucket: "olx-clone-44be2.appspot.com",
-    messagingSenderId: "132069160522",
-    appId: "1:132069160522:web:b13039d8d8c4492e2718b3",
-    measurementId: "G-ZTQDXRN2S0"
+  apiKey: "AIzaSyCYR_rf6w4xdI-_Uc44gShCbUDhIsLRuXw",
+  authDomain: "olx-clone-44be2.firebaseapp.com",
+  projectId: "olx-clone-44be2",
+  storageBucket: "olx-clone-44be2.appspot.com",
+  messagingSenderId: "132069160522",
+  appId: "1:132069160522:web:b13039d8d8c4492e2718b3",
+  measurementId: "G-ZTQDXRN2S0"
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
